b2d9eb8fe2076af8540e6050be8cf2642c569b6b
kuo-hm <hmouraoussama@gmail.com>
2021-08-22 17:53:16 +0100

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins", sans-serif;
+  background-color: #fff;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,100 @@
+<template>
+  <header>
+    <div class="logo">
+      <a href="#" class="nav__logo">Kuo</a>
+    </div>
+    <nav class="nav">
+      <div class="nav__menu" id="nav-menu">
+        <ul class="nav__list">
+          <li>
+            <a href="#home" class="nav__link">Home</a>
+          </li>
+          <li>
+            <a href="#about" class="nav__link">About</a>
+          </li>
+          <li>
+            <a href="#skills" class="nav__link">Skills</a>
+          </li>
+          <li><a href="#work" class="nav__link">Work</a></li>
+          <li><a href="#work" class="nav__link">Contact</a></li>
+        </ul>
+      </div>
+
+      <div class="nav__toggle" id="nav-toggle">
+        <i class="bx bx-menu"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+li,
+a,
+button {
+  font-weight: 500;
+  font-size: 16px;
+  text-decoration: none;
+}
+header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px 10%;
+}
+.logo {
+  cursor: pointer;
+  margin-right: auto;
+}
+.nav__list {
+  list-style: none;
+}
+.nav__list li {
+  display: inline-block;
+  padding: 0px 20px;
+}
+.nav__list li a {
+  transition: all 0.3s ease 0s;
+}
+.nav__list li a:hover {
+  color: rgba(66, 62, 62, 0.712);
+}
+.nav__link:hover {
+  position: relative;
+}
+.nav__link {
+  position: relative;
+}
+
+.nav__link:hover::after {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 0.18rem;
+  left: 0;
+  top: 2rem;
+  background-color: rgba(64, 112, 244, 0.8);
+}
+.link {
+  transition: all 0.3s ease 0s;
+}
+.link:hover {
+  color: rgba(66, 62, 62, 0.712);
+}
+button:hover {
+  background-color: rgba(64, 112, 244, 0.8);
+}
+
+.active::after {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 0.18rem;
+  left: 0;
+  top: 2rem;
+  background-color: #4070f4;
+}
+</style>
